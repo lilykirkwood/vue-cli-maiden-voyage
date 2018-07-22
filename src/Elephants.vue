2a2814e6c17c7b4abeae335cf0b7e4eba7938c11
Lily Kirkwood <lilykirkwood@outlook.com>
2018-07-22 18:10:11 +0100

    removed default app.vue contents - replaced with two components: one imported globally, and the other locally

---
+++ src/Elephants.vue
@@ -0,0 +1,19 @@
+<template>
+  <ol>
+    <li v-for="elephant in elephants">{{ elephant }}</li>
+  </ol>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      elephants: ['Ellie', 'Nelly', 'Dumbo']
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// importing components globally
+import Ninjas from './Ninjas.vue'
+Vue.component('ninjas', Ninjas)
+
 new Vue({
   el: '#app',
   render: h => h(App)
